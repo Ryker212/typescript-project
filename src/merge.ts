735549edc31e968dd680f8b4412e6119dfa820c2
Ryker212 <103985410+Ryker212@users.noreply.github.com>
2024-02-10 00:33:07 +0700

    อธิบายcode

--- src/merge.ts
+++ src/merge.ts
@@ -1,14 +1,14 @@
 export function merge(collection_1: number[], collection_2: number[], collection_3: number[]): number[] {
-    const result: number[] = [];
-    let i = 0, j = 0, k = collection_3.length - 1;
-    while (i < collection_1.length || j < collection_2.length || k >= 0) {
-        const current1 = collection_1[i] || Infinity;
+    const result: number[] = []; //สร้างตัวแปร result มาเก็บค่าแล้วส่งคืน
+    let i = 0, j = 0, k = collection_3.length - 1; //กำหนด ระยะแต่collection_3เป็น -1เนื่องจากเรียงจากมากไปน้อย
+    while (i < collection_1.length || j < collection_2.length || k >= 0) { //ทำงานไปเรื่อยจน collection_1หมดแล้วไป collection_2
+        const current1 = collection_1[i] || Infinity; //กำหนดค่าcurrentต่างๆ ตามcollectionแต่ละช่อง จนหมกcollcyionให้เป็นInfinity
         const current2 = collection_2[j] || Infinity;
         const current3 = collection_3[k];
     
-        const smallest = Math.min(current1, current2, current3);
+        const smallest = Math.min(current1, current2, current3); //หาตัวเลขที่น้องที่สุด แล้วเก็บในsmallest
     
-        if (smallest === current1) {
+        if (smallest === current1) { //เมื่อ smallest เป็นค่าของ currentต่างๆจะทำการ เพื่มค่าจนcurrentนั้นๆถูกกำหนดเป็นInfinity
           i++;
         } else if (smallest === current2) {
           j++;
@@ -16,7 +16,7 @@ export function merge(collection_1: number[], collection_2: number[], collection
           k--;
         }
     
-        result.push(smallest);
+        result.push(smallest); //ใส่ค่าที่น้อยลงใน result
       }
-    return result;
+    return result;//ส่งค่ากลับคืนโปรแกรมที่เรียกfunction
 }
--- test/merge.test.ts
+++ test/merge.test.ts
@@ -1,10 +1,10 @@
 /// <reference types="jest" />
 
-import {merge} from "../src/merge";
+import {merge} from "../src/merge"; //อ้างอิงpath
 test("merge test", () =>{
-    const collection_1 = [1, 3, 5];
-    const collection_2 = [2, 4, 6];
+    const collection_1 = [1, 2, 6]; //สร้างcollection ในการทดสอบ
+    const collection_2 = [3, 4, 5];
     const collection_3 = [9, 7, 0];
-    const expected = [0,1, 2, 3, 4, 5, 6, 7, 9];
-    expect(merge(collection_1, collection_2, collection_3)).toEqual(expected);
+    const expected = [0,1, 2, 3, 4, 5, 6, 7, 9]; //สร้างcollectionที่ถูกต้องหลังmerge มาตรวจสอบผลจากFunction
+    expect(merge(collection_1, collection_2, collection_3)).toEqual(expected); //เปรียบเทียบผลลัพทธ์การทำงาน
 });
